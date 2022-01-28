c43ecc0f9875dc7775c10bb5ae9e40373db088c8
Tim <timothymshores@gmail.com>
2022-01-27 21:17:05 -0700

    Add brute force solution to LC #2006

--- 2006. Count Number of Pairs With Absolute Difference K.js
+++ 2006. Count Number of Pairs With Absolute Difference K.js
@@ -32,3 +32,15 @@
 // 1 <= nums.length <= 200
 // 1 <= nums[i] <= 100
 // 1 <= k <= 99
+
+const countKDifference = (nums, k) => {
+	let count = 0;
+
+	for (let i = 0; i < nums.length; i++) {
+		for (j = i + 1; j < nums.length; j++) {
+			if (nums[i] - nums[j] === k || nums[j] - nums[i] === k) count++;
+		}
+	}
+
+	return count;
+};
