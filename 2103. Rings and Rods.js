5688a06f55af9fa8e4073e24091c057c26ede63e
timothyshores <timothymshores@gmail.com>
2022-02-10 23:06:01 -0700

    Add solution to LC #2103 Rings and Rods

--- 2103. Rings and Rods.js
+++ 2103. Rings and Rods.js
@@ -38,3 +38,44 @@
 // 1 <= n <= 100
 // rings[i] where i is even is either 'R', 'G', or 'B' (0-indexed).
 // rings[i] where i is odd is a digit from '0' to '9' (0-indexed).
+
+/**
+ * @param {string} rings
+ * @return {number}
+ */
+
+const countPoints = (rings) => {
+	// Initial state of every ring
+	const emptyRing = { red: false, green: false, blue: false };
+
+	// Create array of 10 elements with the emptyRing obj
+	const rods = Array(10).fill(emptyRing);
+
+	// Split the input string rings into an array of 2 consecutive characters
+	// E.g. "B0B6G0".match(/.{1,2}/g) returns ['B0', 'B6', 'G0']
+	const pairings = rings.match(/.{1,2}/g);
+
+	// Loop through pairings array
+	for (let pairing of pairings) {
+		// First character in every pairing string is the ring's color
+		const color = pairing[0];
+		// Second character in every pairing string is the position or index of the ring
+		const rodIndex = pairing[1];
+
+		// Set the correspond ring's color flag to true if found in the given pairing
+		if (color === "R") rods[rodIndex] = { ...rods[rodIndex], red: true };
+		if (color === "G") rods[rodIndex] = { ...rods[rodIndex], green: true };
+		if (color === "B") rods[rodIndex] = { ...rods[rodIndex], blue: true };
+	}
+
+	// Initialize a final count of rods with all 3 colors of rings
+	let count = 0;
+
+	// Iterate through rods array
+	for (rod of rods) {
+		// If a given rood has 3 true values then increment the final count
+		if (Object.values(rod).every((boolean) => boolean === true)) count++;
+	}
+
+	return count;
+};
