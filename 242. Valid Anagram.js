642a1a32703555948d6830bebfc0046e290d5cac
timothyshores <timothymshores@gmail.com>
2022-06-18 19:17:43 -0600

    Add solution to LC #242 Valid Anagram using frequency counter pattern

--- 242. Valid Anagram.js
+++ 242. Valid Anagram.js
@@ -12,8 +12,6 @@
 
 // An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
- 
-
 // Example 1:
 
 // Input: s = "anagram", t = "nagaram"
@@ -22,21 +20,44 @@
 
 // Input: s = "rat", t = "car"
 // Output: false
- 
 
 // Constraints:
 
 // 1 <= s.length, t.length <= 5 * 104
 // s and t consist of lowercase English letters.
- 
 
 // Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
 
 /**
- * @param {string} s
- * @param {string} t
+ * @param {string} str1
+ * @param {string} str2
  * @return {boolean}
  */
-const isAnagram = const (str1, str2) => {
-    
+
+const isAnagram = (str1, str2) => {
+	// Store the frequency count of each character in an object
+	const count = {};
+
+	// Handle the condition where strings are different lengths
+	if (str1.length != str2.length) return false;
+
+	// Set count object keys to the unique characters and set values to the frequency count in str1
+	for (char of str1) {
+		count[char] = (count[char] || 0) + 1;
+	}
+
+	// Iterate through the characters in str2
+	for (char of str2) {
+		// If the key is found in count and the value is greater than 0
+		if (count[char] > 0) {
+			// Reduce the frequency count by 1 for the given characters
+			count[char] = count[char] - 1;
+		} else {
+			// The key was not found or the count is negative and return false
+			return false;
+		}
+	}
+
+	// Sum all of the values in count, if the sum is 0 then str1 and str2 are anagrams
+	return Object.values(count).reduce((a, b) => a + b, 0) == 0;
 };
