fb3614a28a564b7eb12fec5f7301e123b5bf36b0
timothyshores <timothymshores@gmail.com>
2022-06-19 22:07:40 -0600

    Add solution to isSubsequence.js

--- Problem Solving Patterns/Two Pointers/isSubsequence.js
+++ Problem Solving Patterns/Two Pointers/isSubsequence.js
@@ -4,7 +4,26 @@
 // Ideal time complexity linear O(N) where N is the number of characters in the second string
 // Ideal space complexity constant O(1)
 
+const isSubsequence = (str1, str2) => {
+	// If str1 is longer than str2 then str1 can NOT be a subsequence of str2
+	if (str1.length > str2.length) return false;
 
+	// Initialize and set two points and count to 0. Constant O(1) space complexity.
+	let [firstPointer, secondPointer, count] = [0, 0, 0];
+
+	// Loop through all characters in second string. Linear O(n) time complexity
+	while (secondPointer < str2.length) {
+		// If character in str1 at index firstPoint is equal to character in str2 at index secondPointer
+		if (str1[firstPointer] === str2[secondPointer]) {
+			// Move first pointer 1 index to the right and increment count
+			[firstPointer++, count++];
+		}
+		// Move second pointer 1 index to the right
+		secondPointer++;
+	}
+
+	return count === str1.length;
+};
 
 // Test Cases should all return true
 console.log(isSubsequence("", "a") === true);
