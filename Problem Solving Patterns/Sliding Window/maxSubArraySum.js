663597f36a05ea6b5b1decbbf54115c36c85fb74
timothyshores <timothymshores@gmail.com>
2022-06-20 01:25:31 -0600

    Add solution to maxSubarrySum.js

--- Problem Solving Patterns/Sliding Window/maxSubArraySum.js
+++ Problem Solving Patterns/Sliding Window/maxSubArraySum.js
@@ -1,7 +1,32 @@
 // Given an array of integers and a number write a function called maxSubarraySum
 // Return the maximum of of a subarray with the length of the number passed into the function
 
+const maxSubarraySum = (arr, n) => {
+	// A subarray can NOT be larger then the original array
+	if (arr.length < n) return null;
 
+	// Initialize a variable called max and set equal to 0
+	let max = 0;
+
+	// Set max to the sum of the first n elements in arr
+	for (let i = 0; i < n; i++) {
+		max += arr[i];
+	}
+
+	// Set current sum equal to the sum of of the n elements in arr
+	let current = max;
+
+	// Loop through the entire array
+	for (let i = 0; i < arr.length - n; i++) {
+		// Subtract the value at index i and add the value at index n
+		current = current - arr[i] + arr[i + n];
+		// If the current sum is greater than the max sum then set the max to current
+		if (current > max) max = current;
+	}
+
+    // Return the maximum sub of a subarray with length n
+	return max;
+};
 
 // Test Cases should all return true
 console.log(maxSubarraySum([1], 1) === 1);
