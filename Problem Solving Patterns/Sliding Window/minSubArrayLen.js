002d79f1bbf8c2ed256ed79586800e5ac39b0ae1
timothyshores <timothymshores@gmail.com>
2022-06-20 22:14:32 -0600

    Add minSubArray.js to Sliding WIndow directory

---
+++ Problem Solving Patterns/Sliding Window/minSubArrayLen.js
@@ -0,0 +1,15 @@
+// Write a function called minSubArrayLen that accepts two parameters
+// First parameter is an array of positive integers and second parameter is a positive integer
+// Return the minimum length of a contiguous subarray where the sum is >= second parameter integer
+// Return 0 if a contiguous subarray with sum >= second parameter integer does NOT exist
+
+
+
+console.log(minSubArrayLen([1], 2) === 0);
+console.log(minSubArrayLen([2, 1, 6, 5, 4], 9) === 2);
+console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7) === 2);
+console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11) === 2);
+console.log(minSubArrayLen([3, 4, 16, 22, 5, 7, 8, 9, 10], 39) === 3);
+console.log(minSubArrayLen([3, 4, 16, 22, 5, 7, 8, 9, 10], 55) === 5);
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95) === 0);
+console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52) === 1);
