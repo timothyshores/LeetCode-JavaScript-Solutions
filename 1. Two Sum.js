283dcca64157171d090e411d2df616d244e490aa
timothyshores <timothymshores@gmail.com>
2022-06-25 00:35:48 -0600

    Add solution to LC #1 Two Sum

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -23,3 +23,33 @@
 // Only one valid answer exists.
 
 // Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+// Solution: O(n) time complexity with O(n) space complexity in the pairings object
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+	// Track the compliment number that sum to target and the index of the initial value
+	let pairings = {};
+	// Track the current number while looping through nums and first index of the current value's pair
+	let number, firstIndex;
+
+	// Loop through nums one time. Linear O(n) time complexity
+	for (let currentIndex = 0; currentIndex < nums.length; currentIndex++) {
+		// Set number to the value of nums at currentIndex
+		number = nums[currentIndex];
+		// Set first index to the value of pairings at the key nums[currentIndex]
+		firstIndex = pairings[number];
+
+		// If a number has a previous number in the nums array that adds up to target
+		if (number in pairings) {
+			// Return firstIndex of the number that adds up to target and currentIndex
+			return [firstIndex, currentIndex];
+		} else {
+			// Set the key as the missing number that adds up to target and value as currentIndex
+			pairings[target - number] = currentIndex;
+		}
+	}
+};
