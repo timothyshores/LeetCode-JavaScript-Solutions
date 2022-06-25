dcdba1693629749dc38e570b9c978ee9447f031d
timothyshores <timothymshores@gmail.com>
2022-06-24 22:12:35 -0600

    Add findLongestSubstring.js

---
+++ Problem Solving Patterns/Sliding Window/findLongestSubstring.js
@@ -0,0 +1,20 @@
+// Write a function called longest substring
+// Accepts a string of letters
+// Returns the length of the longest substring with all distinct characters
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const findLongestSubstring = (str) => {
+
+};
+
+// Test Cases should all return true
+console.log(findLongestSubstring("") === 0);
+console.log(findLongestSubstring("bbbbbb") === 1);
+console.log(findLongestSubstring("rithmschool") === 7);
+console.log(findLongestSubstring("thisisawesome") === 6);
+console.log(findLongestSubstring("thecatinthehat") === 7);
+console.log(findLongestSubstring("thisishowwedoit") === 6);
+console.log(findLongestSubstring("longestsubstring") === 8);
