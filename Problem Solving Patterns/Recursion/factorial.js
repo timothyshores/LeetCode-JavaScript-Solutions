8ca6347128a3c9825a38d146c2f808c343540ed5
timothyshores <timothymshores@gmail.com>
2022-06-26 02:03:56 -0600

    Add factorial.js in recursion directory

---
+++ Problem Solving Patterns/Recursion/factorial.js
@@ -0,0 +1,27 @@
+// Write a function called factorial
+// Input is a positive integer called num
+// return the factorial of a number
+
+// Example 1:
+// Input: num = 2
+// Output: 2
+// Explanation 2 * 1 = 3
+
+// Example 2:
+// Input: num = 3
+// Output: 6
+// Explanation 3 * 2 * 1 = 6
+
+// Example 5:
+// Input: num = 5
+// Output:
+// Explanation 5 * 4 * 3 * 2 * 1 = 120
+
+const factorial = (num) => {};
+
+// ALl test cases return true
+console.log(factorial(1) === 1);
+console.log(factorial(2) === 2);
+console.log(factorial(3) === 6);
+console.log(factorial(4) === 24);
+console.log(factorial(5) === 120);
