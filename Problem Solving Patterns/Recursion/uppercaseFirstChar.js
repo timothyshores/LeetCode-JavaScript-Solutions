0d41b1c3901a1e32feb4bf5c759d97da4eb00052
timothyshores <timothymshores@gmail.com>
2022-06-27 02:39:56 -0600

    Add uppercaseFirstChar problem in recursion directory

---
+++ Problem Solving Patterns/Recursion/uppercaseFirstChar.js
@@ -0,0 +1,8 @@
+// Write  a recursive function called uppercaseArr
+// Input is an array of strings
+// Return the input array of strings with the first character capitalized in each string
+
+// Test Cases
+console.log(uppercaseArr(["sam"]));
+console.log(uppercaseArr(["car", "taco", "banana"]));
+console.log(uppercaseArr(["bob", "sarah", "john"]));
