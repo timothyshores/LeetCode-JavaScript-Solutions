7519fe3959649461cab4cdc5bde3d051a18be965
timothyshores <timothymshores@gmail.com>
2022-07-01 12:14:45 -0600

    Refactor getDigit helper function

--- Problem Solving Patterns/Sorting Algorithms/radixSort.js
+++ Problem Solving Patterns/Sorting Algorithms/radixSort.js
@@ -1,14 +1,14 @@
 const getDigit = (num, index) => {
 	let str = num.toString();
 	if (index >= str.length) return 0;
-	let reversedStr = str.split("").reverse().join("");
-	return parseInt(reversedStr[index], 10);
+	return parseInt(str[--str.length - index]);
 };
 
 // All Test Cases return true
-console.log(getDigit(12345, 0) === 5); // returns
-console.log(getDigit(12345, 1) === 4); // returns
-console.log(getDigit(12345, 2) === 3); // returns
-console.log(getDigit(12345, 3) === 2); // returns
-console.log(getDigit(12345, 4) === 1); // returns
-console.log(getDigit(12345, 5) === 0); // returns
+console.log(getDigit(12345, 0) === 5);
+console.log(getDigit(12345, 1) === 4);
+console.log(getDigit(12345, 2) === 3);
+console.log(getDigit(12345, 3) === 2);
+console.log(getDigit(12345, 4) === 1);
+console.log(getDigit(12345, 5) === 0);
+console.log(getDigit(12345, 6) === 0);
