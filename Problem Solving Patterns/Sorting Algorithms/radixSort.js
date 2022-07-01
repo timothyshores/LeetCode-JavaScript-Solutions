1ca912bcaf4358cc32d28b9e1db339f18ee3428a
timothyshores <timothymshores@gmail.com>
2022-07-01 12:44:39 -0600

    Create digitCount helper function

--- Problem Solving Patterns/Sorting Algorithms/radixSort.js
+++ Problem Solving Patterns/Sorting Algorithms/radixSort.js
@@ -4,6 +4,18 @@ const getDigit = (num, index) => {
 	return parseInt(str[--str.length - index]);
 };
 
+const digitCount = (num) => num.toString().length;
+
+const maxDigits = (nums) => {
+	let maxDigit = digitCount(nums[0]);
+
+	for (let num of nums) {
+		if (maxDigit < digitCount(num)) maxDigit = digitCount(num);
+	}
+
+	return maxDigit;
+};
+
 // All Test Cases return true
 console.log(getDigit(12345, 0) === 5);
 console.log(getDigit(12345, 1) === 4);
@@ -12,3 +24,18 @@ console.log(getDigit(12345, 3) === 2);
 console.log(getDigit(12345, 4) === 1);
 console.log(getDigit(12345, 5) === 0);
 console.log(getDigit(12345, 6) === 0);
+
+console.log(digitCount(0) === 1);
+console.log(digitCount(1) === 1);
+console.log(digitCount(10) === 2);
+console.log(digitCount(100) === 3);
+console.log(digitCount(1000) === 4);
+console.log(digitCount(10000) === 5);
+console.log(digitCount(100000) === 6);
+
+console.log(maxDigits([1]) === 1);
+console.log(maxDigits([1, 10]) === 2);
+console.log(maxDigits([1, 10, 100]) === 3);
+console.log(maxDigits([1, 10, 100, 1000]) === 4);
+console.log(maxDigits([1, 10, 100, 1000, 10000]) === 5);
+console.log(maxDigits([1, 10, 100, 1000, 10000, 100000]) === 6);
