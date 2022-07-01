e2bdce3de92ae62509951e7fff5446d4184e6117
timothyshores <timothymshores@gmail.com>
2022-07-01 11:27:29 -0600

    Added find quicksort implementation

--- Problem Solving Patterns/Sorting Algorithms/quickSort.js
+++ Problem Solving Patterns/Sorting Algorithms/quickSort.js
@@ -4,11 +4,20 @@ const pivotHelper = (arr, start = 0, end = arr.length - 1) => {
 
 	for (let i = start + 1; i < end; i++) {
 		if (arr[i] < pivotValue) {
-            pivotIndex++;
+			pivotIndex++;
 			[[arr[i]], [arr[pivotIndex]]] = [[arr[pivotIndex]], [arr[i]]];
 		}
 	}
 
-    [arr[pivotIndex], [arr[start]]] = [arr[start], [arr[pivotIndex]]];
-    return pivotIndex;
+	[arr[pivotIndex], [arr[start]]] = [arr[start], [arr[pivotIndex]]];
+	return pivotIndex;
+};
+
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+	if (left < right) {
+		let pivotIndex = pivotHelper(arr, left, right);
+		quickSort(arr, left, pivotIndex - 1);
+		quickSort(arr, pivotIndex + 1, right);
+	}
+	return arr;
 };
