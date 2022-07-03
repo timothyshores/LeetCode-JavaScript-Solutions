05681b25baa321c9948cc134dfaa7841781d1ee3
timothyshores <timothymshores@gmail.com>
2022-07-03 10:32:43 -0600

    Add pop method to SLL, helper methods, refactor push and remove tests

--- Data Structures/SinglyLinkedLists/singlyLinkedList.js
+++ Data Structures/SinglyLinkedLists/singlyLinkedList.js
@@ -45,6 +45,24 @@ class SinglyLinkedList {
 	}
 
 	/**
+	 * Returns true if the Singly Linked List has exactly one node
+	 *
+	 * @returns {boolean} returns true if the Linked List has exactly one node
+	 */
+	hasOneNode() {
+		return this.length === 1;
+	}
+
+	/**
+	 * Returns true if the Singly Linked List has two or more nodes
+	 *
+	 * @returns {boolean} returns true if the Linked List has two or more nodes
+	 */
+	hasMultipleNodes() {
+		return this.length >= 2;
+	}
+
+	/**
 	 * Add a new Node at the end of the Singly Linked List
 	 *
 	 * @param {any} value will be stored in the node in the tail of the Singly Linked List
@@ -57,42 +75,55 @@ class SinglyLinkedList {
 		if (this.isEmpty()) {
 			this.head = newNode;
 			this.tail = newNode;
-		} else {
-			// Singly Linked List is NOT empty and has one or more node
-			// Point current tail.next to newNode and update tail to newNode
+		}
+		// Singly Linked List has one or more nodes point current tail to newNode and update tail to newNode
+		if (this.hasOneNode() || this.hasMultipleNodes()) {
 			this.tail.next = newNode;
 			this.tail = newNode;
 		}
+		// Increment length and return the Singly Linked List
 		this.length++;
 		return this;
 	}
+
+	/**
+	 * Remove the tail of the Singly Linked List
+	 *
+	 * @return the tail of the Singly Linked List
+	 */
+	pop() {
+		switch (true) {
+			// Singly Linked List has no nodes return undefined
+			case this.isEmpty():
+				return undefined;
+			// Singly Linked List has exactly one node
+			case this.hasOneNode(): {
+				// Return the node, set head and tail to null and decrement the length
+				let removedNode = this.head;
+				this.head = null;
+				this.tail = null;
+				this.length--;
+				return removedNode;
+			}
+			// Singly Linked List has two or more nodes
+			case this.hasMultipleNodes(): {
+				// Create newTail variable and set to head
+				let newTail = this.head;
+				// Traverse the Singly Linked List from head to the last node before the tail
+				for (let i = 0; i < this.length - 2; i++) {
+					newTail = newTail.next;
+				}
+				// Create temp variable to return the current tail
+				let removedNode = this.tail;
+				// Set the second to the last node's pointer to null
+				newTail.next = null;
+				// Set the new tail to the second to the last node
+				this.tail = newTail;
+				// Decrement the length of the Singly Linked List
+				this.length--;
+				// Return the original tail node
+				return removedNode;
+			}
+		}
+	}
 }
-
-// All Test Cases Return true
-let sll = new SinglyLinkedList();
-console.log(sll.length == 0);
-console.log(sll.head == null);
-console.log(sll.tail == null);
-
-console.log(sll.push("1"));
-console.log(sll.length === 1);
-console.log("head", sll.head.value == 1);
-console.log("tail", sll.tail.value == 1);
-console.log("tail", sll.tail.next == null);
-
-console.log(sll.push("2"));
-console.log(sll.length === 2);
-console.log(sll.head.value == 1);
-console.log(sll.tail.value == 2);
-console.log(sll.tail.next == null);
-console.log(sll.head.next.value == 2);
-console.log(sll.head.next.next == null);
-
-console.log(sll.push("3"));
-console.log(sll.length === 3);
-console.log(sll.head.value == 1);
-console.log(sll.tail.value == 3);
-console.log(sll.tail.next == null);
-console.log(sll.head.next.value == 2);
-console.log(sll.head.next.next.value == 3);
-console.log(sll.head.next.next.next === null);
