b322496f3f946d8a69603595a22138ba9fc1b56d
timothyshores <timothymshores@gmail.com>
2022-11-05 23:50:03 -0700

    Add helper functions to DLL class

--- Data Structures/DoublyLinkedLists/doublyLinkedList.js
+++ Data Structures/DoublyLinkedLists/doublyLinkedList.js
@@ -12,4 +12,31 @@ class DoublyLinkedList {
 		this.tail = null;
 		this.length = 0;
 	}
+
+	/**
+	 * Returns true if the Doubly Linked List (DLL) is empty
+	 *
+	 * @returns {boolean} returns true if the DLL is empty
+	 */
+	isEmpty() {
+		return this.length === 0;
+	}
+
+	/**
+	 * Returns true if the Doubly Linked List has exactly one node
+	 *
+	 * @returns {boolean} returns true if the DLL has exactly one node
+	 */
+	hasOneNode() {
+		return this.length === 1;
+	}
+
+	/**
+	 * Returns true if the Doubly Linked List (DLL) has two or more nodes
+	 *
+	 * @returns {boolean} returns true if the DLL has two or more nodes
+	 */
+	hasMultipleNodes() {
+		return this.length >= 2;
+	}
 }
