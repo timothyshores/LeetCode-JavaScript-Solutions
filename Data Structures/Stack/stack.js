3cd716cd04bb18c0b579aaf4346ec20b607b877b
timothyshores <timothymshores@gmail.com>
2022-11-08 14:13:51 -0700

    Add Stack and Node constructors

---
+++ Data Structures/Stack/stack.js
@@ -0,0 +1,14 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class Stack {
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
+}
