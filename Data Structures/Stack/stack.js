92786028309db06340faebdb7a1b3e8372082bb2
timothyshores <timothymshores@gmail.com>
2022-11-08 14:40:32 -0700

    Implement push and pop in stack data structure class

--- Data Structures/Stack/stack.js
+++ Data Structures/Stack/stack.js
@@ -11,4 +11,38 @@ class Stack {
 		this.last = null;
 		this.size = 0;
 	}
+
+	isEmpty = () => this.size === 0;
+
+	hasOneNode = () => this.size === 1;
+
+	push(value) {
+		let newNode = new Node(value);
+
+		if (this.isEmpty()) {
+			this.first = newNode;
+			this.last = newNode;
+		} else {
+			newNode.next = this.first;
+			this.first = newNode;
+		}
+
+		return ++this.size;
+	}
+
+	pop() {
+		if (this.isEmpty()) return null;
+
+		let firstNode = this.first;
+
+		if (this.hasOneNode()) {
+			this.first = null;
+			this.last = null;
+		} else {
+			this.first = firstNode.next;
+		}
+
+		this.size--;
+		return firstNode.value;
+	}
 }
