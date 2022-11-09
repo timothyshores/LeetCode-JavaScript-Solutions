2ca75f954d7fb1c7d5a03daee5bacb71f943f9de
timothyshores <timothymshores@gmail.com>
2022-11-08 19:14:46 -0700

    Create binary search tree class and constructor

---
+++ Data Structures/BinarySearchTree/binarySearchTree.js
@@ -0,0 +1,13 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class BinarySearchTree {
+	constructor() {
+		this.root = null;
+	}
+}
