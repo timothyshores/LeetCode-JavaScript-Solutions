e199f4a970ffd4b010016706b1fb75cdc2c92b8f
timothyshores <timothymshores@gmail.com>
2022-11-08 17:27:43 -0700

    Add enqueue method and helper functions

--- Data Structures/Queue/queue.js
+++ Data Structures/Queue/queue.js
@@ -11,4 +11,21 @@ class Queue {
 		this.last = null;
 		this.size = 0;
 	}
+
+	isEmpty = () => this.size === 0;
+	hasOneNode = () => this.size === 1;
+
+	enqueue(value) {
+		let newNode = new Node(value);
+
+		if (this.isEmpty()) {
+			this.first = newNode;
+			this.last = newNode;
+		} else {
+			this.last.next = newNode;
+			this.last = newNode;
+		}
+
+		return ++this.size;
+	}
 }
