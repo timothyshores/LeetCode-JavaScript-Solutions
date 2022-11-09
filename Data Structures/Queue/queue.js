c25e3ce4e3722b839a9aeb7f07f820d8b0716587
timothyshores <timothymshores@gmail.com>
2022-11-08 17:22:51 -0700

    Create Queue and Node constructors

---
+++ Data Structures/Queue/queue.js
@@ -0,0 +1,14 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+class Queue {
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
+}
