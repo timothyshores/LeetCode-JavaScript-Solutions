7e2b9144d56ce7bc21c1693e6eb0094d4755b546
timothyshores <timothymshores@gmail.com>
2022-11-13 15:29:08 -0700

    Add find method test cases

--- Data Structures/BinarySearchTree/findTestCases.js
+++ Data Structures/BinarySearchTree/findTestCases.js
@@ -80,3 +80,86 @@ class BinarySearchTree {
 		}
 	}
 }
+
+// Initialize and instantiate new binary search tree called bst
+const bst = new BinarySearchTree();
+
+// Call find on an empty BST returns false
+console.log(bst.find(1)); // false;
+console.log(bst.find(0)); // false;
+console.log(bst.find(10)); // false;
+console.log(bst.find(101)); // false;
+
+// Insert 41 as the root in in BST
+bst.insert(41);
+console.log(bst.find(41)); // true;
+
+// Insert 20 as the left child of the root node
+//      41
+//     /
+//   20
+
+bst.insert(20);
+console.log(bst.find(20)); // true;
+
+// Insert 65 as the right child of the root node
+//      41
+//     /  \
+//   20    65
+
+bst.insert(65);
+console.log(bst.find(65)); // true;
+
+// Insert 11 as the left child of 20
+//        41
+//       /  \
+//     20    65
+//    /
+//  11
+
+bst.insert(11);
+console.log(bst.find(11)); // true;
+
+// Insert 29 as the right child of 20
+//        41
+//       /  \
+//     20    65
+//    /  \
+//  11    29
+
+bst.insert(29);
+console.log(bst.find(29)); // true;
+
+// Insert 50 as the left child of 65
+//          41
+//         /   \
+//       /      \
+//     20        65
+//    /  \      /
+//  11    29  50
+
+bst.insert(50);
+console.log(bst.find(50)); // true;
+
+// Insert 91 as the left child of 65
+//          41
+//         /   \
+//       /      \
+//     20        65
+//    /  \      /  \
+//  11    29  50    91
+
+bst.insert(91);
+console.log(bst.find(91)); // true;
+
+// Call find on number values that do not exist in the BST returns false
+console.log(bst.find(10)); // false;
+console.log(bst.find(12)); // false;
+console.log(bst.find(21)); // false;
+console.log(bst.find(30)); // false;
+console.log(bst.find(42)); // false;
+console.log(bst.find(63)); // false;
+console.log(bst.find(66)); // false;
+console.log(bst.find(53)); // false;
+console.log(bst.find(90)); // false;
+console.log(bst.find(93)); // false;
