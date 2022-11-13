0a8b3acd44524c2337bd9a82eab5abe3c39af90f
timothyshores <timothymshores@gmail.com>
2022-11-12 23:20:00 -0700

    Add insert method implementation to BST test cases

--- Data Structures/BinarySearchTree/insertTestCases.js
+++ Data Structures/BinarySearchTree/insertTestCases.js
@@ -10,4 +10,34 @@ class BinarySearchTree {
 	constructor() {
 		this.root = null;
 	}
+
+	insert(value) {
+		let node = new Node(value);
+
+		// If no root node then made the new node the root node
+		if (!this.root) {
+			this.root = node;
+		} else {
+			// BST has a root node
+			let currentNode = this.root;
+
+			while (currentNode) {
+				if (value > currentNode.value) {
+					if (currentNode.right) currentNode = currentNode.right;
+					else {
+						currentNode.right = node;
+						break;
+					}
+				} else {
+					if (currentNode.left) currentNode = currentNode.left;
+					else {
+						currentNode.left = node;
+						break;
+					}
+				}
+			}
+		}
+
+		return this;
+	}
 }
