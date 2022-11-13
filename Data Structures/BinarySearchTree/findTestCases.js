30a5eec6b93c573addb4dcb5755e55bb9efde6d8
timothyshores <timothymshores@gmail.com>
2022-11-13 14:26:46 -0700

    Create find test cases JS file and add existing BST class

---
+++ Data Structures/BinarySearchTree/findTestCases.js
@@ -0,0 +1,45 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class BinarySearchTree {
+	constructor() {
+		this.root = null;
+	}
+
+	insert(value) {
+		let node = new Node(value);
+
+		// If no root node then made the new node the root node
+		if (!this.root) {
+			this.root = node;
+		} else {
+			// BST has a root node
+			let currentNode = this.root;
+
+			while (currentNode) {
+				if (value > currentNode.value) {
+					if (currentNode.right) currentNode = currentNode.right;
+					else {
+						currentNode.right = node;
+						break;
+					}
+				} else if (value < currentNode.value) {
+					if (currentNode.left) currentNode = currentNode.left;
+					else {
+						currentNode.left = node;
+						break;
+					}
+				} else {
+					return undefined;
+				}
+			}
+		}
+
+		return this;
+	}
+}
