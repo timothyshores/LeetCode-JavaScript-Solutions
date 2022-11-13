e0a63053f8df4ef29df85d5cce997656a09715a1
timothyshores <timothymshores@gmail.com>
2022-11-13 15:35:12 -0700

    Add find method in BST class

--- Data Structures/BinarySearchTree/binarySearchTree.js
+++ Data Structures/BinarySearchTree/binarySearchTree.js
@@ -42,4 +42,47 @@ class BinarySearchTree {
 
 		return this;
 	}
+
+	/**
+	 * Search for a value in the BST
+	 *
+	 * @param {any} value that method is looking for in the BST
+	 * @return Returns true if a node with a specific value is in the BST else returns false
+	 */
+	find(value) {
+		// Start at the root node
+		let currentNode = this.root;
+
+		// Check if there's a root node, if there's no root then the search is completed
+		if (this.root === null) return false;
+
+		// If there is a root
+		while (true) {
+			// Compare the value we're look for to the value of the root if the values are equal return true
+			if (value === currentNode.value) return true;
+
+			// If the value we're looking for is greater than value of the root
+			if (value > currentNode.value) {
+				// Check if the root has a right child node
+				if (currentNode.right) {
+					// If the right child node exists then update the current node to be the right child current node
+					currentNode = currentNode.right;
+				} else {
+					// If the right child node does NOT exist return false
+					return false;
+				}
+			}
+			// If the value we're looking for is less than value of the root
+			if (value < currentNode.value) {
+				// Check if the root has a left child node
+				if (currentNode.left) {
+					// If the left child node exists then update the current node to be the left child current node
+					currentNode = currentNode.left;
+				} else {
+					// If the left child node does NOT exist return false
+					return false;
+				}
+			}
+		}
+	}
 }
