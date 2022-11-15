8ea795ed750bedb89df06daf5e20d8888f719770
timothyshores <timothymshores@gmail.com>
2022-11-14 22:27:50 -0700

    Create breadth first search file and add pseudocode

---
+++ Data Structures/Trees/Tree Traversal/breadthFirstSearch.js
@@ -0,0 +1,9 @@
+// Create a helper queue
+// Add the root to the queue
+// Start a loop that keeps going until the queue is empty
+// Dequeue the front head node from the queue
+// Push the value of that node into our final return array
+// If there's a left properly on the node that was just dequeued then add it to the queue
+// If there's a right properly on the node that was just dequeued then add it to the queue
+// Return the final array
+
