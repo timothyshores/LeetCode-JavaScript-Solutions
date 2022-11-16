10025ae1592ec8b63b03a5a3a13ee86f103f4b54
timothyshores <timothymshores@gmail.com>
2022-11-16 12:51:20 -0700

    Add pseudocode for extractMax method

--- Data Structures/Heap/extractMaxTestCases.js
+++ Data Structures/Heap/extractMaxTestCases.js
@@ -34,5 +34,27 @@ class MaxBinaryHeap {
 		}
 
 		return val;
-	}
+    }
+    
+    extractMax() {
+        // Swap the first and last element in this.values;
+            // Remove the root which is the max value
+            // Replace the root with the value element in this.values
+        // Call .pop() on this.values to return the last element or the node with the largest value in the BMH
+
+        // Check the root node's left and right child
+            // Parent index is 0
+        // Find the max value between the two child nodes
+            // Left child index is parent index * 2 + 1 && less than this.values.length
+            // Right child index is parent index * 2 + 2 && less than this.values.length
+            // If the left child node's value is greater than the right child node's value
+                // Swap the root with the left child node
+        // Same thing for the right child node
+            // If the right child node's value is greater than the left child node's value
+                // Swap the root with the right child node
+        // Continue swapping the last node that was bubbled up to the top until it's in the right spot
+        // The correct location for the bubbled up node is when it's value is less than it's parent node's value
+            // Another way to phrase this is when the parent node's value is greater than the child node's value
+        // Return the root
+    }
 }
