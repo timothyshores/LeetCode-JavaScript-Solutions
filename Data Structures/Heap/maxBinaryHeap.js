e9a98433cc991b55bbc86cc75a2b6142f859ca52
timothyshores <timothymshores@gmail.com>
2022-11-15 23:51:38 -0700

    Implement insert method in max binary heap class

--- Data Structures/Heap/maxBinaryHeap.js
+++ Data Structures/Heap/maxBinaryHeap.js
@@ -2,4 +2,38 @@ class MaxBinaryHeap {
 	constructor() {
 		this.values = [];
 	}
+
+	insert(insertedValue) {
+		// Push value to the end of values array
+		this.values.push(insertedValue);
+
+		// Get the index of the new value pushed into the values array
+		let childIndex = this.values.length - 1;
+
+		// Get index of the parent of the new value pushed into the array
+		let parentIndex = Math.floor((childIndex - 1) / 2);
+
+		// Get value of the parent of the new value pushed into the array
+		let parentNodeValue = this.values[parentIndex];
+
+		// If parent node's value is less than the newly inserted value then bubble up
+		while (parentNodeValue < insertedValue) {
+			// Swap parent and child nodes in values array
+			[this.values[childIndex], this.values[parentIndex]] = [
+				this.values[parentIndex],
+				this.values[childIndex],
+			];
+
+			// Update child node to the current parent node index
+			childIndex = parentIndex;
+
+			// Use formula to update new parent node index
+			parentIndex = Math.floor((childIndex - 1) / 2);
+
+			// Update parentNodeValue variable to determine if loop should repeat
+			parentNodeValue = this.values[parentIndex];
+		}
+
+		return this.values;
+	}
 }
