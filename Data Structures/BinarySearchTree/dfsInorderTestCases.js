c3b4e9f3f63e9e91db98b9c55c7a7ac51b3486d2
timothyshores <timothymshores@gmail.com>
2022-11-15 17:03:22 -0700

    Add DFS inorder test cases file and add existing BST class

---
+++ Data Structures/BinarySearchTree/dfsInorderTestCases.js
@@ -0,0 +1,186 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class BinarySearchTree {
+	constructor() {
+		this.root = null;
+	}
+
+	/**
+	 * Insert a node into the BST with the value passed in as a parameter
+	 *
+	 * @param {any} value of the node in the BST
+	 * @return The node inserted into the BST
+	 */
+	insert(value) {
+		let node = new Node(value);
+
+		// If no root node then made the new node the root node
+		if (!this.root) {
+			this.root = node;
+		} else {
+			// BST has a root node
+			let currentNode = this.root;
+
+			while (currentNode) {
+				if (value > currentNode.value) {
+					if (currentNode.right) currentNode = currentNode.right;
+					else {
+						currentNode.right = node;
+						break;
+					}
+				} else if (value < currentNode.value) {
+					if (currentNode.left) currentNode = currentNode.left;
+					else {
+						currentNode.left = node;
+						break;
+					}
+				} else {
+					return undefined;
+				}
+			}
+		}
+
+		return this;
+	}
+
+	/**
+	 * Find the node with a given value in the BST
+	 *
+	 * @param {any} value that method is looking for in the BST
+	 * @return Returns node with a given node if value is in the BST else return false
+	 */
+	find(value) {
+		// Start at the root node
+		let currentNode = this.root;
+
+		// Check if there's a root node, if there's no root then the search is completed
+		if (this.root === null) return false;
+
+		// If there is a root
+		while (true) {
+			// Compare the value we're look for to the value of the root if the values are equal return currentNode
+			if (value === currentNode.value) return currentNode;
+
+			// If the value we're looking for is greater than value of the root
+			if (value > currentNode.value) {
+				// Check if the root has a right child node
+				if (currentNode.right) {
+					// If the right child node exists then update the current node to be the right child current node
+					currentNode = currentNode.right;
+				} else {
+					// If the right child node does NOT exist return false
+					return false;
+				}
+			}
+
+			// If the value we're looking for is less than value of the root
+			if (value < currentNode.value) {
+				// Check if the root has a left child node
+				if (currentNode.left) {
+					// If the left child node exists then update the current node to be the left child current node
+					currentNode = currentNode.left;
+				} else {
+					// If the left child node does NOT exist return false
+					return false;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Traverse the BST using breadth first search algorithm
+	 *
+	 * @return Returns an array in breadth first search order
+	 */
+	breadthFirstSearch() {
+		// Create a helper queue for nodes to be visited
+		const unvisitedNodes = [];
+
+		// Create a pointer and set to the root node
+		let currentNode = this.root;
+
+		// Add the root to the helper queue
+		unvisitedNodes.push(currentNode);
+
+		// Create another queue for nodes that have been visited in the BSF
+		const visitedNodes = [];
+
+		// Start a loop that keeps going until the helper queue is empty
+		while (unvisitedNodes?.length) {
+			// Dequeue the front head node from the queue and update pointer variable
+			currentNode = unvisitedNodes.shift();
+
+			// Push the value of that node into the other queue that keeps track of the visited nows
+			visitedNodes.push(currentNode.value);
+
+			// If there's a left properly on the current node then add it to the queue
+			if (currentNode.left) unvisitedNodes.push(currentNode.left);
+
+			// If there's a right properly on the current node then add it to the queue
+			if (currentNode.right) unvisitedNodes.push(currentNode.right);
+		}
+
+		// Return the final array
+		return visitedNodes;
+	}
+
+	/**
+	 * Traverse the BST using depth first search preorder
+	 *
+	 * @return Returns an array in depth first search preorder
+	 */
+	dfsPreorder() {
+		// Create variable to keep track of the visited nodes
+		const visited = [];
+
+		// Create recursive helper function that takes in a node as an input
+		const traverse = (node) => {
+			// Add the node to the list our visited notes
+			visited.push(node.value);
+
+			// If there's a left child node call the helper function
+			if (node.left) traverse(node.left);
+
+			// If there's a right child node call the helper function
+			if (node.right) traverse(node.right);
+		};
+
+		// Call helper function with the root node
+		traverse(this.root);
+
+		return visited;
+	}
+
+	/**
+	 * Traverse the BST using depth first search postorder
+	 *
+	 * @return Returns an array in depth first search postorder
+	 */
+	dfsPostorder() {
+		// Create variable to keep track of the visited nodes
+		const visited = [];
+
+		// Create recursive helper function that takes in a node as an input
+		const traverse = (node) => {
+			// If there's a left child node call the helper function
+			if (node.left) traverse(node.left);
+
+			// If there's a right child node call the helper function
+			if (node.right) traverse(node.right);
+
+			// Add the node to the list our visited notes
+			visited.push(node.value);
+		};
+
+		// Call helper function with the root node
+		traverse(this.root);
+
+		return visited;
+	}
+}
