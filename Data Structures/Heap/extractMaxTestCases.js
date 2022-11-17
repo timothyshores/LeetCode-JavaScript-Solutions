987a766c878a3f0980229c53450842f0b3327c55
timothyshores <timothymshores@gmail.com>
2022-11-16 22:31:12 -0700

    Implement my solution to extract max

--- Data Structures/Heap/extractMaxTestCases.js
+++ Data Structures/Heap/extractMaxTestCases.js
@@ -34,27 +34,79 @@ class MaxBinaryHeap {
 		}
 
 		return val;
-    }
-    
-    extractMax() {
-        // Swap the first and last element in this.values;
-            // Remove the root which is the max value
-            // Replace the root with the value element in this.values
-        // Call .pop() on this.values to return the last element or the node with the largest value in the BMH
+	}
 
-        // Check the root node's left and right child
-            // Parent index is 0
-        // Find the max value between the two child nodes
-            // Left child index is parent index * 2 + 1 && less than this.values.length
-            // Right child index is parent index * 2 + 2 && less than this.values.length
-            // If the left child node's value is greater than the right child node's value
-                // Swap the root with the left child node
-        // Same thing for the right child node
-            // If the right child node's value is greater than the left child node's value
-                // Swap the root with the right child node
-        // Continue swapping the last node that was bubbled up to the top until it's in the right spot
-        // The correct location for the bubbled up node is when it's value is less than it's parent node's value
-            // Another way to phrase this is when the parent node's value is greater than the child node's value
-        // Return the root
-    }
+	extractMax() {
+		const val = this.values;
+
+		let lastIndex = val.length - 1;
+
+		// Swap the first and last element in this.values;
+		[([val[0], val[lastIndex]] = [val[lastIndex], val[0]])];
+
+		// Call .pop() on this.values to return the last element or the node with the largest value in the BMH
+		const maxValue = this.values.pop();
+
+		// Parent index is 0 which is the new root node after the swap
+		let parentIndex = 0;
+		const parentValue = val[0];
+
+		// Left child index is parent index * 2 + 1 && less than this.values.length
+		const getLeftChildIndex = (index) => {
+			if (index * 2 + 1 < val.length) return index * 2 + 1;
+			return null;
+		};
+
+		// Call getLeftChildIndex with the parent index and create variable for left child value
+		let leftChildIndex = getLeftChildIndex(parentIndex);
+		let leftChildValue = val[leftChildIndex];
+
+		// Right child index is parent index * 2 + 2 && less than this.values.length
+		const getRightChildIndex = (index) => {
+			if (index * 2 + 2 < val.length) return index * 2 + 2;
+			return null;
+		};
+
+		// Call getRightChildIndex with the parent index and create variable for right child value
+		let rightChildIndex = getRightChildIndex(parentIndex);
+		let rightChildValue = val[rightChildIndex];
+
+		// Return the larger of the two values either left child value or right child value
+		const getLargestChildValue = () =>
+			leftChildValue > rightChildValue ? leftChildValue : rightChildValue;
+
+		// Call getLargestChildValue()
+		let largestChildValue = getLargestChildValue();
+
+		// Continue to loop as long as the new root new is less than either its left or right child node's value
+		while (parentValue < largestChildValue) {
+			// If the left child's value is greater than the parent value and the right child value
+			if (leftChildValue > parentValue && leftChildValue > rightChildValue) {
+				// Swap parent node with left child node and update parent index
+				val[parentIndex] = leftChildValue;
+				val[leftChildIndex] = parentValue;
+				parentIndex = leftChildIndex;
+			}
+			// If the right child's value is greater than the parent value and the left child value
+			if (rightChildValue > parentValue && rightChildValue > leftChildValue) {
+				// Swap parent node with right child node and update parent index
+				val[parentIndex] = rightChildValue;
+				val[rightChildIndex] = parentValue;
+				parentIndex = rightChildIndex;
+			}
+
+			leftChildIndex = getLeftChildIndex(parentIndex);
+			rightChildIndex = getRightChildIndex(parentIndex);
+
+			leftChildValue = val[leftChildIndex];
+			rightChildValue = val[rightChildIndex];
+
+			largestChildValue = getLargestChildValue();
+		}
+
+		console.log(val);
+
+		// Return the root
+		return maxValue;
+	}
 }
