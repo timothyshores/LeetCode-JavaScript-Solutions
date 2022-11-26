301d54d7c964d4b7a3722affcb6c9a63a4702bb2
timothyshores <timothymshores@gmail.com>
2022-11-26 13:53:47 -0700

    Add solution to LC #896 monotonic array

--- 896. Monotonic Array.js
+++ 896. Monotonic Array.js
@@ -17,4 +17,36 @@
 // Input: nums = [1,3,2]
 // Output: false
 
-const isMonotonic = (array) => {};
+const isMonotonic = (array) => {
+	if (array.length < 2) return true;
+
+	let decreasing = false;
+	let increasing = false;
+	let index = 1;
+
+	while (index < array.length) {
+		if (array[0] < array[index]) {
+			increasing = true;
+			break;
+		}
+		if (array[0] > array[index]) {
+			decreasing = true;
+			break;
+        }
+        
+		if (index === array.length - 1) return true;
+
+		index++;
+	}
+
+	let previousElement, currentElement;
+
+	for (let i = index + 1; i < array.length; i++) {
+		previousElement = array[i - 1];
+		currentElement = array[i];
+		if (increasing && previousElement > currentElement) return false;
+		if (decreasing && previousElement < currentElement) return false;
+	}
+
+	return true;
+};
