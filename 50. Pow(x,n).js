3d9ada9b3f63c6f86a908d0d3784a1bfeeed5413
timothyshores <timothymshores@gmail.com>
2023-01-20 17:52:43 -0700

    Add solution to LC 50 Pow(x,n)

---
+++ 50. Pow(x,n).js
@@ -0,0 +1,46 @@
+/*
+
+Implement pow(x, n), which calculates x raised to the power n(i.e., xn).
+
+Example 1:
+    Input: x = 2.00000, n = 10
+    Output: 1024.00000
+
+Example 2:
+    Input: x = 2.10000, n = 3
+    Output: 9.26100
+
+Example 3:
+    Input: x = 2.00000, n = -2
+    Output: 0.25000
+    Explanation: 2-2 = 1/22 = 1/4 = 0.25
+
+Constraints:
+    -100.0 < x < 100.0
+    -231 <= n <= 231-1
+    n is an integer.
+    -104 <= xn <= 104
+*/
+
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+const myPow = (x, n) => {
+	const helper = (x, n) => {
+		if (n === 0 || x === 1) return 1;
+		if (x === 0) return 0;
+
+		const half = helper(x, Math.floor(n / 2));
+		const nIsOdd = n % 2 === 1;
+		const nIsEven = n % 2 === 0;
+
+		if (nIsOdd) return half * half * x;
+		if (nIsEven) return half * half;
+	};
+
+	if (n < 0) return 1 / helper(x, Math.abs(n));
+
+	return helper(x, Math.abs(n));
+};
--- README.md
+++ README.md
@@ -7,6 +7,7 @@ LeetCode Profile: https://leetcode.com/timothyshores/
 | :-------------- | :-------------- |:---- | 
 |[1. Two Sum](https://leetcode.com/problems/two-sum/)|Easy|[JavaScript](1.%20Two%20Sum.js)
 |[3. Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)|Medium|[JavaScript](3.%20Longest%20Substring%20Without%20Repeating%20Characters.js)
+|[50. Pow(x, n)](https://leetcode.com/problems/powx-n/)|Medium|[JavaScript]50.%20Pow(x,n).js)
 |[121. Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/)|Easy|[JavaScript](121.%20Best%20Time%20to%20Buy%20and%20Sell%20Stock.js)
 |[125. Valid Palindrome](https://leetcode.com/problems/valid-palindrome/)|Easy|[JavaScript](125.%20Valid Palindrome.js)
 |[136. Single Number](https://leetcode.com/problems/single-number/)|Easy|[JavaScript](136.%2Single%2Number.js)
