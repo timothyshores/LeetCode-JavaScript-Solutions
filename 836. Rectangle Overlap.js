3c2d264dda038644e21890a67368f0021d9def38
timothyshores <timothymshores@gmail.com>
2023-01-29 19:24:26 -0700

    Add solution to LC 836 rectangle overlap and update readme

--- 836. Rectangle Overlap.js
+++ 836. Rectangle Overlap.js
@@ -31,4 +31,13 @@ Constraints:
  * @param {number[]} rec2
  * @return {boolean}
  */
-const isRectangleOverlap = (rec1, rec2) => {};
+const isRectangleOverlap = (rec1, rec2) => {
+	const [r1x1, r1y1, r1x2, r1y2] = rec1;
+	const [r2x1, r2y1, r2x2, r2y2] = rec2;
+
+	if (r2x1 >= r1x2 || r2x2 <= r1x1 || r2y1 >= r1y2 || r2y2 <= r1y1) {
+		return false;
+	}
+
+	return true;
+};
