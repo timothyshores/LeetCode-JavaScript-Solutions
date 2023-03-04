89cfa08bb8415521847ffa0f0f86638ff3bc30f4
timothyshores <timothymshores@gmail.com>
2023-03-04 13:02:04 -0700

    Add explore, brainstorm and plan notes to LC 455 Reshape the Matrix

--- 566. Reshape the Matrix.js
+++ 566. Reshape the Matrix.js
@@ -31,15 +31,55 @@ Constraints:
 
 🔎 EXPLORE
 What are some other insightful & revealing test cases?
+ 
+Input mat is an array of arrays
+- Each subarray represents a row
+- The elements in a the subarray are values
+
+Input r represents numbers of rows in the output array
+Input c represents number of columns in the output array
 
 🧠 BRAINSTORM
 What approaches could work?
-Algorithm 1: 
-Time: O() 
-Space: O() 
+Algorithm 1: Create a new array and fill with the input values
+Time: O(N) to traverse the original input array
+Space: O(N) to store the new final results array
 
 📆 PLAN
 Outline of algorithm #: 
+
+Need to verify that inputs r and c are valid requests
+
+A valid request must have an equal number of elements in both the input array mat and output array
+
+Need to compare the number of elements in both the input and output 2D arrays
+
+const inputLength = mat.reduce((a, b) => a + b.length, 0);
+
+if (inputLength !== r * c) return mat
+
+Create a m x n matrix
+
+const result = Array(m).fill().map(() =>Array(n).fill())
+
+Traverse the input array keeping track of the current row and column
+
+let row = 0;
+let column = 0;
+
+for (const subarray of mat) {
+    for (const element of subarray) {
+        // const represents every element in 
+        result[r][c] = element
+        column++;
+        if (column === r) {
+            column = 0;
+            row++;
+        }
+    }
+}
+
+return result;
 
 🛠️ IMPLEMENT
 Write your algorithm */
