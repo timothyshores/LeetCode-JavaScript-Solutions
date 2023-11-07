d9c3383dbebca8ad9d572d43cf030fbbd634031c
Timothy Shores <timothymshores@gmail.com>
2023-11-07 11:50:59 -0700

    Add engineering method notes to LC 187

--- 187. Repeated DNA Sequences.js
+++ 187. Repeated DNA Sequences.js
@@ -21,4 +21,54 @@ Constraints:
     1 <= s.length <= 105
     s[i] is either 'A', 'C', 'G', or 'T'.
 
+ENGINEERING METHOD
+
+EXPLORE
+
+Want to return the unique 10 letter long substrings that occur 2 or more times in input string s
+
+BRAINSTORM
+
+Approach 1: Brute force nested for loops
+Time: O(n^2)
+Space: O(k) where k is the length of the output or results array
+
+Aproach 2: Use 2 sets
+Time: O(N)
+Space: O(N) if every 10 letter sequence is unique stored in set
+
+PLAN
+
+Aproach #2
+
+Initialize 2 empty sets
+    - allSequences
+        - Store 10 letter long strings in this set
+        - Check if the current 10 letter long sequence has the same sequence in this set
+    - duplicateSequences 
+        - Store all unique 10-letter-long sequences (substrings) that occur more than once in a DNA molecule
+
+const allSequences = new Set();
+const duplicateSequences = new Set();
+
+// Handle edge case where s is 10 letters long or less
+if (s.length <= 10) return [];
+
+for (let i = 0; i < s.length - 9; i++) {
+    // Get the current 10-letter-long sequence starting at index i
+    const currentSequence = s.substring(i, i + 10);
+
+    // Check if our all sequences set has the current sequence
+    if (allSequences.has(currentSequence)) {
+        // currentSequence is a duplicate and occures more than once
+        duplicateSequences.add(currentSequence);
+    } else {
+        // currentSequence is unique. Add to our all sequences set
+        allSequences.add(currentSequence)
+    }
+}
+
+// Convert the duplicateSequences set into an arry and return the array
+return [...duplicateSequences];
+
 */
