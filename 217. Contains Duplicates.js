2ae28d6d7b504915b90d93490c362a857a77d3f4
Timothy Shores <timothymshores@gmail.com>
2023-12-02 23:17:02 -0700

    Refactor contains duplicates implementation

--- 217. Contains Duplicates.js
+++ 217. Contains Duplicates.js
@@ -1,24 +1,43 @@
-// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+/*
 
-// Example 1:
-// Input: nums = [1,2,3,1]
-// Output: true
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
-// Example 2:
-// Input: nums = [1,2,3,4]
-// Output: false
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
 
-// Example 3:
-// Input: nums = [1,1,1,3,3,4,3,2,4,2]
-// Output: true
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
 
-// Constraints:
-// 1 <= nums.length <= 105
-// -109 <= nums[i] <= 109
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
 
+Constraints:
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+
+*/
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
+const containsDuplicate = (nums) => {
+	const uniqueNumbers = new Set();
 
-const containsDuplicate = (nums) => new Set(nums).size !== nums.length;
+	// Iterate through the nums array
+	for (const num of nums) {
+		// If current number is not contained in the set
+		if (!uniqueNumbers.has(num)) {
+			// Add current number to the set
+			uniqueNumbers.add(num);
+		} else {
+			// Current number is already contained the the set
+			return true;
+		}
+	}
+
+	// If we've iterated through all nums then all nums are unique
+	return false;
+};
