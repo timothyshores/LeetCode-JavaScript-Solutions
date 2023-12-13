d4e5159c60d67fe36e4df0731e3293b7a92e6360
Timothy Shores <timothymshores@gmail.com>
2023-12-13 00:07:17 -0700

    Add LC 103 prompt with solution and update README

---
+++ 101. Symmetric Tree.js
@@ -0,0 +1,63 @@
+/*
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+Example 1:
+    Input: root = [1,2,2,3,4,4,3]
+    Output: true
+
+Example 2:
+    Input: root = [1,2,2,null,3,null,3]
+    Output: false
+
+Constraints:
+    The number of nodes in the tree is in the range [1, 1000].
+    -100 <= Node.val <= 100
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * Check if two nodes are symmetric.
+ * @param {TreeNode} left
+ * @param {TreeNode} right
+ * @return {boolean}
+ */
+const areNodesSymmetric = (left, right) => {
+    return (!left && !right) || (left && right && left.val === right.val);
+};
+
+/**
+ * Check if a binary tree is symmetric.
+ * @param {TreeNode} root - The root of the binary tree.
+ * @return {boolean} - True if the tree is symmetric, false otherwise.
+ */
+const isSymmetric = (root) => {
+    // Edge case: empty or null tree
+    if (!root) return true;
+
+    /**
+     * Helper function to check symmetry recursively.
+     * @param {TreeNode} left
+     * @param {TreeNode} right
+     * @return {boolean}
+     */
+    const helper = (left, right) => {
+        // Base case: the left and right nodes do NOT exist
+        if (!left && !right) return true;
+
+        // The left and right nodes are NOT symmetric
+        if (!areNodesSymmetric(left, right)) return false;
+
+        // Recursive case: call the function on the left and right child nodes
+        return helper(left.left, right.right) && helper(left.right, right.left);
+    };
+
+    return helper(root.left, root.right);
+};
