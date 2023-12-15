ea7d9a8525c94232b3f2f2892d8d4d398e062388
Timothy Shores <timothymshores@gmail.com>
2023-12-15 14:47:00 -0700

    Implement LC 94 using iterative stack

--- 94. Binary Tree Inorder Traversal.js
+++ 94. Binary Tree Inorder Traversal.js
@@ -62,3 +62,22 @@ const inorderTraversalDfsHelper = (root) => {
 	dfs(root);
 	return result;
 };
+
+const inorderTraversalIterative = (root) => {
+	const stack = [];
+	const result = [];
+	let current = root;
+
+	while (current || stack.length) {
+		while (current) {
+			stack.push(current);
+			current = current.left;
+		}
+
+		current = stack.pop();
+		result.push(current.val);
+		current = current.right;
+	}
+
+	return result;
+};
