b2855582e17ef397d4bf2a3ebfc229657903821f
timothyshores <timothymshores@gmail.com>
2024-02-12 08:18:26 -0700

    Add solution for LC 344

--- 344. Reverse String.js
+++ 344. Reverse String.js
@@ -18,3 +18,31 @@ Constraints:
     s[i] is a printable ascii character.
 
 */
+
+/**
+ * @param {string} str
+ * @param {number} p1
+ * @param {number} p2
+ * @return {string}
+ */
+const swapChars = (str, p1, p2) => {
+	[str[p1], str[p2]] = [str[p2], str[p1]];
+	return str;
+};
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+const reverseString = (s) => {
+	let left = 0;
+	let right = s.length - 1;
+
+	while (left < right) {
+		s = swapChars(s, left, right);
+		left++;
+		right--;
+	}
+
+	return s;
+};
