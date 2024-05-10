40a365544752cb9aa06b0cf8229a957fd4a47359
Timothy Shores <timothymshores@gmail.com>
2024-05-10 14:02:20 -0600

    Update LC 896 solution

--- 896. Monotonic Array.js
+++ 896. Monotonic Array.js
@@ -1,52 +1,42 @@
-// 896. Monotonic Array\
-// An array is monotonic if it is either monotone increasing or monotone decreasing.
+/*
 
-// An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+896. Monotonic Array\
+An array is monotonic if it is either monotone increasing or monotone decreasing.
 
-// Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
 
-// Example 1:
-// Input: nums = [1,2,2,3]
-// Output: true
+Given an integer array nums, return true if the given array is monotonic, or false otherwise.
 
-// Example 2:
-// Input: nums = [6,5,4,4]
-// Output: true
+Example 1:
+Input: nums = [1,2,2,3]
+Output: true
 
-// Example 3:
-// Input: nums = [1,3,2]
-// Output: false
+Example 2:
+Input: nums = [6,5,4,4]
+Output: true
 
-const isMonotonic = (array) => {
-	if (array.length < 2) return true;
+Example 3:
+Input: nums = [1,3,2]
+Output: false
 
-	let decreasing = false;
-	let increasing = false;
-	let index = 1;
+*/
 
-	while (index < array.length) {
-		if (array[0] < array[index]) {
-			increasing = true;
-			break;
-		}
-		if (array[0] > array[index]) {
-			decreasing = true;
-			break;
-        }
-        
-		if (index === array.length - 1) return true;
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const isMonotonic = nums => {
+    let increasing, decreasing;
 
-		index++;
-	}
+    for (let i = 1; i < nums.length; i++) {
+        const prev = nums[i - 1];
+        const curr = nums[i];
 
-	let previousElement, currentElement;
+        if (prev < curr) increasing = true;
+        if (prev > curr) decreasing = true;
+        if (decreasing && increasing) return false;
+    }
 
-	for (let i = index + 1; i < array.length; i++) {
-		previousElement = array[i - 1];
-		currentElement = array[i];
-		if (increasing && previousElement > currentElement) return false;
-		if (decreasing && previousElement < currentElement) return false;
-	}
-
-	return true;
+    return true;
 };
+
