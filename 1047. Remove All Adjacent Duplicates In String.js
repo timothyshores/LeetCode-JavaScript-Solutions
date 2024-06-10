b9fd37999e1ba3fd8e8be46ea335bc3bca99543f
Timothy Shores <timothymshores@gmail.com>
2024-06-09 20:12:39 -0600

    Implement LC 1047. Remove All Adjacent Duplicates In String using a stack

--- 1047. Remove All Adjacent Duplicates In String.js
+++ 1047. Remove All Adjacent Duplicates In String.js
@@ -30,4 +30,12 @@ s consists of lowercase English letters.
  * @param {string} s
  * @return {string}
  */
-const removeDuplicates = (s) => {};
+const removeDuplicates = (s) => {
+  const stack = [];
+
+  for (const char of s) {
+    stack[stack.length - 1] === char ? stack.pop() : stack.push(char);
+  }
+
+  return stack.join("");
+};
