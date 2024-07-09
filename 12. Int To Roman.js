2db1ce6c56b114a13199273d4bcf5e037a2a3e48
Timothy Shores <timothymshores@gmail.com>
2024-07-08 23:37:26 -0600

    Update LC 12. Integer to Roman prompt and solution

--- 12. Int To Roman.js
+++ 12. Int To Roman.js
@@ -11,44 +11,43 @@ C             100
 D             500
 M             1000
 
-For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+Roman numerals are formed by appending the conversions of decimal place values from highest to lowest. Converting a decimal place value into a Roman numeral has the following rules:
 
-Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
-
-I can be placed before V (5) and X (10) to make 4 and 9. 
-X can be placed before L (50) and C (100) to make 40 and 90. 
-C can be placed before D (500) and M (1000) to make 400 and 900.
-Given an integer, convert it to a roman numeral.
+- If the value does not start with 4 or 9, select the symbol of the maximal value that can be subtracted from the input, append that symbol to the result, subtract its value, and convert the remainder to a Roman numeral.
+- If the value starts with 4 or 9 use the subtractive form representing one symbol subtracted from the following symbol, for example, 4 is 1 (I) less than 5 (V): IV and 9 is 1 (I) less than 10 (X): IX. Only the following subtractive forms are used: 4 (IV), 9 (IX), 40 (XL), 90 (XC), 400 (CD) and 900 (CM).
+- Only powers of 10 (I, X, C, M) can be appended consecutively at most 3 times to represent multiples of 10. You cannot append 5 (V), 50 (L), or 500 (D) multiple times. If you need to append a symbol 4 times use the subtractive form.
 
 */
 
-const INT_TO_ROMAN_NUMERALS_LOOKUP_TABLE = [
-	[1000, "M"],
-	[900, "CM"],
-	[500, "D"],
-	[400, "CD"],
-	[100, "C"],
-	[90, "XC"],
-	[50, "L"],
-	[40, "XL"],
-	[10, "X"],
-	[9, "IX"],
-	[5, "V"],
-	[4, "IV"],
-	[1, "I"],
+const romanNumeralsTuples = [
+  [1000, "M"],
+  [900, "CM"],
+  [500, "D"],
+  [400, "CD"],
+  [100, "C"],
+  [90, "XC"],
+  [50, "L"],
+  [40, "XL"],
+  [10, "X"],
+  [9, "IX"],
+  [5, "V"],
+  [4, "IV"],
+  [1, "I"],
 ];
 
-const intToRoman = (intToConvert) => {
-	let romanNumeralResults = "";
+/**
+ * @param {number} num
+ * @return {string}
+ */
+const intToRoman = (num) => {
+  let result = "";
 
-	for (let [
-		currentInt,
-		currentRomanNumeral,
-	] of INT_TO_ROMAN_NUMERALS_LOOKUP_TABLE) {
-		let count = Math.floor(intToConvert / currentInt);
-		romanNumeralResults += currentRomanNumeral.repeat(count);
-		intToConvert %= currentInt;
-	}
+  for (const [value, numeral] of romanNumeralsTuples) {
+    while (num >= value) {
+      result += numeral;
+      num -= value;
+    }
+  }
 
-	return romanNumeralResults;
+  return result;
 };
